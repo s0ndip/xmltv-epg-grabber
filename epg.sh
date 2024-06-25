a2c19e0bcdd00269cfbfa44a11aaae6340311a17
s0ndip <s0ndip@outlook.com>
2024-06-25 12:34:49 +0530

    Update epg.sh

--- epg.sh
+++ epg.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 # Run the PHP commands
-php tempest.php --epg config=tempest.config.xml invgz
+php tempest.php --epg config=jiotv.config.xml invgz
+php tempest.php --epg config=tataplay.config.xml invgz
 
 # Remove the specified XML files
-rm tempest_config/epg/tempest_original.xml
-rm tempest_config/epg/tempest.xml
+rm tempest_config/epg/jiotv_original.xml
+rm tempest_config/epg/jiotv.xml
+rm tempest_config/epg/tataplay_original.xml
+rm tempest_config/epg/tataplay.xml
 
 # Optional: print a message indicating the script has finished
 echo "Script execution completed and specified files have been removed."
